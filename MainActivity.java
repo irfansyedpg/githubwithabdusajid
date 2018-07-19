98d72cd0015282adeaa976e623cbdb90d3cef241
syed.irfanullah <irfansyed.pg@gmail.com>
2018-07-19 17:30:59 +0500

    irfan syed

---
+++ MainActivity.java
@@ -0,0 +1,17 @@
+package com.irfansyed.githubwithabdusajid;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        // hello irfan syed
+
+    }
+}
